065e80fe38aac890aa170bb50e8f523f6706ff97
Anton <Anton@ANTON-NOTEBOOK>
2016-11-30 19:48:26 +0900

    Delete 3 test functions.

--- WebServiceProject/WebServiceProject/WebService.asmx.cs
+++ WebServiceProject/WebServiceProject/WebService.asmx.cs
@@ -37,50 +37,6 @@ namespace WebServiceProject
             }
         }
 
-
-        [WebMethod]
-        public DataTable InsertUser(int userid, string nombre)
-        {
-            int numRows = 0;
-            DataTable table = new DataTable();
-            DBConn MyConnection = new DBConn();
-            MyConnection.Connection_ToDB();
-
-            // SqlParameter useridParam = new SqlParameter("userid", System.Data.SqlDbType.NChar,1000);
-            // SqlParameter nombreParam = new SqlParameter("nombre", System.Data.SqlDbType.VarChar,200);
-            // useridParam.Value = userid;
-            // nombreParam.Value = nombre;
-            SqlCommand comm = new SqlCommand();
-            comm.Connection = DBConn.conn;
-            comm.CommandText = "INSERT INTO Usuario (userid,nombre) VALUES (@useridParam,@nombreParam)";
-            comm.Parameters.AddWithValue("@useridParam", userid);
-            comm.Parameters.AddWithValue("@nombreParam", nombre);
-            numRows=comm.ExecuteNonQuery();
-            table.Columns.Add("Rows", typeof(int));
-            table.Rows.Add(numRows);
-            MyConnection.SqlConnectionClose();
-
-            return table;
-
-        }
-
-        [WebMethod]
-        public DataSet GetUserById(int UserID)
-        {
-            DataSet ds = new DataSet();
-            DBConn MyConnection = new DBConn();
-
-            MyConnection.Connection_ToDB();
-            SqlCommand comm = new SqlCommand();
-            comm.Connection = DBConn.conn;
-            comm.Parameters.Add("id", SqlDbType.Int).Value = UserID;
-            comm.CommandText = "SELECT * FROM Usuario WHERE userid = (@id)";
-            SqlDataAdapter adp = new SqlDataAdapter();
-            adp.SelectCommand = comm;
-            adp.Fill(ds);
-            return ds;
-        }
-
         [WebMethod]
         public DataSet GetSaldoActualizado(string email, string password)
         {
@@ -128,7 +84,6 @@ namespace WebServiceProject
             comm.Parameters.AddWithValue("@montoTransaccion", montoTransaccion);
             */
 
-
             numRows = comm.ExecuteNonQuery();
             table.TableName = "Table";
             table.Columns.Add("Rows", typeof(int));
@@ -139,30 +94,5 @@ namespace WebServiceProject
             return ds;
 
         }
-
-        [WebMethod]
-        // public DataSet AndroidTest(int userid)
-        public DataSet AndroidTest()
-        {
-            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["BDTRansactionConnectionString"].ConnectionString))
-            {
-                using (SqlCommand comm = new SqlCommand("SP_Test", conn)) {
-
-                    comm.CommandType = CommandType.StoredProcedure;
-
-                    //comm.Parameters.Add("userid", SqlDbType.Int).Value = userid;
-                    comm.Parameters.Add("userid", SqlDbType.Int).Value = 1;
-                    SqlDataAdapter adp = new SqlDataAdapter();
-                    adp.SelectCommand = comm;
-
-                    DataSet ds = new DataSet();
-                    ds.GetXml();
-                    adp.Fill(ds);
-                    count++;
-                    return ds;
-                }
-            }
-        }
-
     }
 }
