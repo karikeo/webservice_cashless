2d09d8026cf4c3675f597107ebf9487c8d176335
Anton <Anton@ANTON-NOTEBOOK>
2017-01-19 21:30:16 +0900

    Test3

--- WebServiceProject/WebService.asmx.cs
+++ WebServiceProject/WebService.asmx.cs
@@ -13,7 +13,8 @@ namespace WebServiceProject
     /// <summary>
     /// Summary description for WebService
     /// </summary>
-    [WebService(Namespace = "http://badplanet.ddns.net/")]
+    //[WebService(Namespace = "http://badplanet.ddns.net/")]
+    [WebService(Namespace = "http://www.vendomatica.cl/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
@@ -34,50 +35,6 @@ namespace WebServiceProject
             {
                 conn.Close();
             }
-        }
-
-
-        [WebMethod]
-        public DataTable InsertUser(int userid, string nombre)
-        {
-            int numRows = 0;
-            DataTable table = new DataTable();
-            DBConn MyConnection = new DBConn();
-            MyConnection.Connection_ToDB();
-
-            // SqlParameter useridParam = new SqlParameter("userid", System.Data.SqlDbType.NChar,1000);
-            // SqlParameter nombreParam = new SqlParameter("nombre", System.Data.SqlDbType.VarChar,200);
-            // useridParam.Value = userid;
-            // nombreParam.Value = nombre;
-            SqlCommand comm = new SqlCommand();
-            comm.Connection = DBConn.conn;
-            comm.CommandText = "INSERT INTO Usuario (userid,nombre) VALUES (@useridParam,@nombreParam)";
-            comm.Parameters.AddWithValue("@useridParam", userid);
-            comm.Parameters.AddWithValue("@nombreParam", nombre);
-            numRows=comm.ExecuteNonQuery();
-            table.Columns.Add("Rows", typeof(int));
-            table.Rows.Add(numRows);
-            MyConnection.SqlConnectionClose();
-
-            return table;
-
-        }
-
-        [WebMethod]
-        public DataSet GetUserById(int UserID)
-        {
-            DataSet ds = new DataSet();
-            DBConn MyConnection = new DBConn();
-
-            MyConnection.Connection_ToDB();
-            SqlCommand comm = new SqlCommand();
-            comm.Connection = DBConn.conn;
-            comm.Parameters.Add("id", SqlDbType.Int).Value = UserID;
-            comm.CommandText = "SELECT * FROM Usuario WHERE userid = (@id)";
-            SqlDataAdapter adp = new SqlDataAdapter();
-            adp.SelectCommand = comm;
-            adp.Fill(ds);
-            return ds;
         }
 
         [WebMethod]
@@ -103,7 +60,7 @@ namespace WebServiceProject
         public DataSet NewTransaction(string email, int tipoTransaccion, string montoTransaccion)
         {
             int numRows = 0;
-            
+
             DataTable table = new DataTable();
 
             decimal mTransaccion = 0;
@@ -127,55 +84,15 @@ namespace WebServiceProject
             comm.Parameters.AddWithValue("@montoTransaccion", montoTransaccion);
             */
 
-
-#if true
             numRows = comm.ExecuteNonQuery();
             table.TableName = "Table";
             table.Columns.Add("Rows", typeof(int));
             table.Rows.Add(numRows);
-#else
-            using (SqlDataReader reader = comm.ExecuteReader())
-            {
-                if (reader.HasRows)
-                {
-                    if (reader.Read())
-                    {
-                        object[] fields = new object[reader.FieldCount];
-                        reader.GetSqlValues(fields);
-                    }
-                }
-            }
-#endif
             DataSet ds = new DataSet();
             ds.Tables.Add(table);
             MyConnection.SqlConnectionClose();
             return ds;
 
         }
-
-        [WebMethod]
-        // public DataSet AndroidTest(int userid)
-        public DataSet AndroidTest()
-        {
-            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["BDTRansactionConnectionString"].ConnectionString))
-            {
-                using (SqlCommand comm = new SqlCommand("SP_Test", conn)) {
-
-                    comm.CommandType = CommandType.StoredProcedure;
-
-                    //comm.Parameters.Add("userid", SqlDbType.Int).Value = userid;
-                    comm.Parameters.Add("userid", SqlDbType.Int).Value = 1;
-                    SqlDataAdapter adp = new SqlDataAdapter();
-                    adp.SelectCommand = comm;
-
-                    DataSet ds = new DataSet();
-                    ds.GetXml();
-                    adp.Fill(ds);
-                    count++;
-                    return ds;
-                }
-            }
-        }
-
     }
 }
